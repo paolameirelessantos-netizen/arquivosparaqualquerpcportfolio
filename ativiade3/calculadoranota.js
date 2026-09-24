6c0b1108b8fe8e184bd19865939cf376deaa9b37
paolameirelessantos-netizen <paola.meireles.santos@escola.pr.gov.br>
2026-09-24 09:42:05 -0300

    Add files via upload

--- ativiade3/calculadoranota.js
+++ ativiade3/calculadoranota.js
@@ -4,7 +4,16 @@ let resultado;
 
 function calcular(){
     nota1trim = Number (prompt("Digite a nota de seu primeiro trimestre:"))
-    nota2trim = Number (prompt("Digite a nota de seu primeiro trimestre:"))
+    nota2trim = Number (prompt("Digite a nota de seu segundo trimestre:"))
 
-    resultado = 180 - nota1trim + nota2trim;
+    resultado = 180 - (nota1trim + nota2trim);
+   
+    if(resultado <= 0){
+        alert("Parabéns iniciante! Você está aprovado(a), ter um por cento da minha sabedoria dá nisso");
+    } 
+    
+    else{
+        alert( "Você ainda precisa tirar "  + resultado +  " no terceiro trimestre para ser aprovado, vc insulta a minha inteligência");
+    }
 }
+ 
